d478907f1ec1045b25c3d1664f7f02900b0155e0
Nathaly Michel Berroa Fermín <nathalyberroaf@gmail.com>
2025-04-05 14:25:17 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // ⬅️ importante para usar next export
+    images: {
+        unoptimized: true, // ⬅️ esto soluciona el problema de _next/image en producción
+    },
+};
 
 export default nextConfig;
